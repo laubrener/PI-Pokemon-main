ea58fbf26e72e429eb14109cdf3b84af4e34dfaa
laubrener <79546818+laubrener@users.noreply.github.com>
2022-03-13 20:25:52 -0300

    CSS landing page

--- client/src/components/Detail.jsx
+++ client/src/components/Detail.jsx
@@ -4,7 +4,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { getDetail } from "../actions";
 import { useEffect } from "react";
 
-
+// [ ] Los campos mostrados en la ruta principal para cada pokemon (imagen, nombre y tipos)
+// [ ] Número de Pokemon (id)
+// [ ] Estadísticas (vida, fuerza, defensa, velocidad)
+// [ ] Altura y peso
 export default function Detail(props){
     const dispatch = useDispatch();
     const { id } = useParams();
@@ -21,8 +24,15 @@ export default function Detail(props){
             {
                 myPokemon.length ?
                 <div>
-                    <h1>Soy {myPokemon[0].name}</h1>
+                    <h4>Pokemon number {myPokemon[0].id}</h4>
+                    <h1>{myPokemon[0].name}</h1>
                     <img src={myPokemon[0].image} alt="Imagen" />
+                    <h4>HP: {myPokemon[0].hp}</h4>
+                    <h4>Speed: {myPokemon[0].speed}</h4>
+                    <h4>Attack: {myPokemon[0].attack}</h4>
+                    <h4>Defense: {myPokemon[0].defense}</h4>
+                    <h4>Height: {myPokemon[0].height}</h4>
+                    <h4>Weight: {myPokemon[0].weight}</h4>
                     <h4>Type: {myPokemon[0].types.map(el => el.name + (' '))}</h4>
                 </div>
                 : <p>Loading...</p>
--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from './LandingPage.module.css';
 
 export default function LandingPage(){
     return(
         <div>
-            <img className="background-image" src="https://wallpapercave.com/wp/XSbCZ4F.jpg" alt="imagen"/>
-            <h1>Bienvenidos a mi pagina</h1>
+            <img className="background-image" src="https://media.vandal.net/i/1200x630/10-2021/2021105724573_1.jpg" alt="imagen"/>
             <Link to ='/home'>
-                <button>Ingresar</button>
+                <button className={styles.btn}>Catch 'em all</button>
             </Link>
         </div>
     );
